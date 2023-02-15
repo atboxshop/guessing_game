ed1f2dc756835c6271881e0885472bd44a646b62
atboxshop <atboxshop@gmail.com>
2023-02-15 10:58:23 -0500

    change

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+extern crate rand;
+
+use std::io;
+use rand::Rng;
+use rand::thread_rng;
+use std::cmp::Ordering;
+
+fn main() 
+{
+    println!("Guess the number!");
+    let mut rng = thread_rng();
+    let secret_number = rng.gen_range(1..101);
+    println!("The secret number is: {}", secret_number);
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("Your guess: {}", guess);
+        match guess.cmp(&secret_number)
+        {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
+    }
+}
+
